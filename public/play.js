595a5e09c9db9c97fe1bd8646694fa014d14fd7b
Lee <lee.jensen@agilix.com>
2023-02-28 12:42:22 -0700

    fix(play) enable safari to play sounds without user permission

--- public/play.js
+++ public/play.js
@@ -2,126 +2,142 @@
 const GameEndEvent = 'gameEnd';
 const GameStartEvent = 'gameStart';
 
-class Button {
-  constructor(soundUrl, el) {
-    this.el = el;
-    this.sound = loadSound(soundUrl);
+const btnDescriptions = [
+  { file: 'sound1.mp3', hue: 120 },
+  { file: 'sound2.mp3', hue: 0 },
+  { file: 'sound3.mp3', hue: 60 },
+  { file: 'sound4.mp3', hue: 240 },
+];
 
-    this.press = async function (delayMs = 500, playSound = true) {
-      el.style.filter = 'brightness(100%)';
-      if (playSound) {
-        this.sound.play();
-      }
-      await delay(delayMs);
-      el.style.filter = 'brightness(50%)';
-      await delay(100);
-    };
+class Button {
+  constructor(description, el) {
+    this.el = el;
+    this.hue = description.hue;
+    this.sound = loadSound(description.file);
+    this.paint(25);
+  }
+
+  paint(level) {
+    const background = `hsl(${this.hue}, 100%, ${level}%)`;
+    this.el.style.backgroundColor = background;
+  }
+
+  async press(volume) {
+    this.paint(50);
+    await this.play(volume);
+    this.paint(25);
+  }
+
+  // Work around Safari's rule to only play sounds if given permission.
+  async play(volume = 1.0) {
+    this.sound.volume = volume;
+    await new Promise((resolve) => {
+      this.sound.onended = resolve;
+      this.sound.play();
+    });
   }
 }
 
 class Game {
-  #buttons;
-  #allowPlayer;
-  #sequence;
-  #playerPlaybackPos;
-  #mistakeSound;
-  #socket;
+  buttons;
+  allowPlayer;
+  sequence;
+  playerPlaybackPos;
+  mistakeSound;
+  socket;
 
   constructor() {
-    this.#buttons = new Map();
-    this.#allowPlayer = false;
-    this.#sequence = [];
-    this.#playerPlaybackPos = 0;
-    this.#mistakeSound = loadSound('error.mp3');
+    this.buttons = new Map();
+    this.allowPlayer = false;
+    this.sequence = [];
+    this.playerPlaybackPos = 0;
+    this.mistakeSound = loadSound('error.mp3');
 
-    const sounds = ['sound1.mp3', 'sound2.mp3', 'sound3.mp3', 'sound4.mp3'];
     document.querySelectorAll('.game-button').forEach((el, i) => {
-      if (i < sounds.length) {
-        this.#buttons.set(el.id, new Button(sounds[i], el));
-        el.style.filter = 'brightness(50%)';
+      if (i < btnDescriptions.length) {
+        this.buttons.set(el.id, new Button(btnDescriptions[i], el));
       }
     });
 
     const playerNameEl = document.querySelector('.player-name');
-    playerNameEl.textContent = this.#getPlayerName();
+    playerNameEl.textContent = this.getPlayerName();
 
-    this.#configureWebSocket();
+    this.configureWebSocket();
   }
 
   async pressButton(button) {
-    if (this.#allowPlayer) {
-      this.#allowPlayer = false;
-      await this.#buttons.get(button.id).press();
+    if (this.allowPlayer) {
+      this.allowPlayer = false;
+      await this.buttons.get(button.id).press(1.0);
 
-      if (this.#sequence[this.#playerPlaybackPos].el.id === button.id) {
-        this.#playerPlaybackPos++;
-        if (this.#playerPlaybackPos === this.#sequence.length) {
-          this.#playerPlaybackPos = 0;
-          this.#addNote();
-          this.#updateScore(this.#sequence.length - 1);
-          await this.#playSequence(500);
+      if (this.sequence[this.playerPlaybackPos].el.id === button.id) {
+        this.playerPlaybackPos++;
+        if (this.playerPlaybackPos === this.sequence.length) {
+          this.playerPlaybackPos = 0;
+          this.addButton();
+          this.updateScore(this.sequence.length - 1);
+          await this.playSequence();
         }
-        this.#allowPlayer = true;
+        this.allowPlayer = true;
       } else {
-        this.#saveScore(this.#sequence.length - 1);
-        this.#mistakeSound.play();
-        await this.#buttonDance();
+        this.saveScore(this.sequence.length - 1);
+        this.mistakeSound.play();
+        await this.buttonDance(2);
       }
     }
   }
 
   async reset() {
-    this.#allowPlayer = false;
-    this.#playerPlaybackPos = 0;
-    this.#sequence = [];
-    this.#updateScore('00');
-    await this.#buttonDance(1);
-    this.#addNote();
-    await this.#playSequence(1000);
-    this.#allowPlayer = true;
+    this.allowPlayer = false;
+    this.playerPlaybackPos = 0;
+    this.sequence = [];
+    this.updateScore('--');
+    await this.buttonDance(1);
+    this.addButton();
+    await this.playSequence();
+    this.allowPlayer = true;
 
     // Let other players know a new game has started
-    this.#broadcastEvent(this.#getPlayerName(), GameStartEvent, {});
+    this.broadcastEvent(this.getPlayerName(), GameStartEvent, {});
   }
 
-  async #playSequence(delayMs = 0) {
-    if (delayMs > 0) {
-      await delay(delayMs);
-    }
-    for (const btn of this.#sequence) {
-      await btn.press();
-    }
-  }
-
-  #getPlayerName() {
+  getPlayerName() {
     return localStorage.getItem('userName') ?? 'Mystery player';
   }
 
-  #addNote() {
-    const btn = this.#getRandomButton();
-    this.#sequence.push(btn);
+  async playSequence() {
+    await delay(500);
+    for (const btn of this.sequence) {
+      await btn.press(1.0);
+      await delay(100);
+    }
   }
 
-  #updateScore(score) {
+  addButton() {
+    const btn = this.getRandomButton();
+    this.sequence.push(btn);
+  }
+
+  updateScore(score) {
     const scoreEl = document.querySelector('#score');
     scoreEl.textContent = score;
   }
 
-  async #buttonDance(laps = 5) {
+  async buttonDance(laps = 1) {
     for (let step = 0; step < laps; step++) {
-      for (const btn of this.#buttons.values()) {
-        await btn.press(100, false);
+      for (const btn of this.buttons.values()) {
+        await btn.press(0.0);
       }
     }
   }
 
-  #getRandomButton() {
-    let buttons = Array.from(this.#buttons.values());
-    return buttons[Math.floor(Math.random() * this.#buttons.size)];
+  getRandomButton() {
+    let buttons = Array.from(this.buttons.values());
+    return buttons[Math.floor(Math.random() * this.buttons.size)];
   }
 
-  async #saveScore(score) {
-    const userName = this.#getPlayerName();
+  async saveScore(score) {
+    const userName = this.getPlayerName();
     const date = new Date().toLocaleDateString();
     const newScore = { name: userName, score: score, date: date };
 
@@ -133,18 +149,18 @@ class Game {
       });
 
       // Let other players know the game has concluded
-      this.#broadcastEvent(userName, GameEndEvent, newScore);
+      this.broadcastEvent(userName, GameEndEvent, newScore);
 
       // Store what the service gave us as the high scores
       const scores = await response.json();
       localStorage.setItem('scores', JSON.stringify(scores));
     } catch {
       // If there was an error then just track scores locally
-      this.#updateScoresLocal(newScore);
+      this.updateScoresLocal(newScore);
     }
   }
 
-  #updateScoresLocal(newScore) {
+  updateScoresLocal(newScore) {
     let scores = [];
     const scoresText = localStorage.getItem('scores');
     if (scoresText) {
@@ -173,39 +189,38 @@ class Game {
 
   // Functionality for peer communication using WebSocket
 
-  #configureWebSocket() {
+  configureWebSocket() {
     const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
-    this.#socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
-    this.#socket.onopen = (event) => {
-      this.#displayMsg('system', 'game', 'connected');
+    this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+    this.socket.onopen = (event) => {
+      this.displayMsg('system', 'game', 'connected');
     };
-    this.#socket.onclose = (event) => {
-      this.#displayMsg('system', 'game', 'disconnected');
+    this.socket.onclose = (event) => {
+      this.displayMsg('system', 'game', 'disconnected');
     };
-    this.#socket.onmessage = async (event) => {
+    this.socket.onmessage = async (event) => {
       const msg = JSON.parse(await event.data.text());
       if (msg.type === GameEndEvent) {
-        this.#displayMsg('player', msg.from, `scored ${msg.value.score}`);
+        this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
       } else if (msg.type === GameStartEvent) {
-        this.#displayMsg('player', msg.from, `started a new game`);
+        this.displayMsg('player', msg.from, `started a new game`);
       }
     };
   }
 
-  #displayMsg(cls, from, msg) {
+  displayMsg(cls, from, msg) {
     const chatText = document.querySelector('#player-messages');
     chatText.innerHTML =
-      `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` +
-      chatText.innerHTML;
+      `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
   }
 
-  #broadcastEvent(from, type, value) {
+  broadcastEvent(from, type, value) {
     const event = {
       from: from,
       type: type,
       value: value,
     };
-    this.#socket.send(JSON.stringify(event));
+    this.socket.send(JSON.stringify(event));
   }
 }
 
